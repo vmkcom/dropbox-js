6dfb5f0bf7ae644ee628b899e9b27161f639a321
Bryan Bishop <kanzure@gmail.com>
2013-02-03 17:00:55 -0600

    fix cdn url to dropbox.min.js for checkbox.js
    This updates the url in the vendor.sh script for the checkbox.js
    example.

--- samples/checkbox.js/vendor.sh
+++ samples/checkbox.js/vendor.sh
@@ -7,7 +7,5 @@ curl http://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.3.3/coffee-script.min
     > public/lib/coffee-script.js
 curl http://cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js \
     > public/lib/less.js
-curl http://cdnjs.cloudflare.com/ajax/libs/dropbox/0.8.2/dropbox.js \
-    > public/lib/dropbox.js
-curl http://cdnjs.cloudflare.com/ajax/libs/dropbox/0.8.2/dropbox.min.js \
+curl http://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.9.0/dropbox.min.js \
     > public/lib/dropbox.min.js
