f241461575aba5751f385c02dbdc8fcd1c0ded65
Victor Costan <costan@gmail.com>
2013-02-07 17:08:35 -0500

    Updated checkbox.js dependencies.

--- samples/checkbox.js/vendor.sh
+++ samples/checkbox.js/vendor.sh
@@ -1,11 +1,17 @@
 mkdir -p public/lib
-curl http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.js \
-    > public/lib/jquery.js
-curl http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js \
     > public/lib/jquery.min.js
-curl http://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.3.3/coffee-script.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.js \
+    > public/lib/jquery.js
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.map \
+    > public/lib/jquery.min.map
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.4.0/coffee-script.min.js \
     > public/lib/coffee-script.js
-curl http://cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js \
     > public/lib/less.js
-curl http://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.9.0/dropbox.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.9.0/dropbox.min.js \
     > public/lib/dropbox.min.js
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.9.0/dropbox.js \
+    > public/lib/dropbox.js
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.9.0/dropbox.min.map \
+    > public/lib/dropbox.min.map
