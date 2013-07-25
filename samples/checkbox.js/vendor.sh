c0c5a698666b59a78b38ce71d654f6d109b8d27a
Victor Costan <costan@gmail.com>
2013-07-24 17:26:56 -0700

    Updated versions for vendored libraries in checkbox.js sample.

--- samples/checkbox.js/vendor.sh
+++ samples/checkbox.js/vendor.sh
@@ -1,13 +1,13 @@
 mkdir -p public/lib
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js \
     > public/lib/jquery.min.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js \
     > public/lib/jquery.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.map \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.map \
     > public/lib/jquery.min.map
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.2/coffee-script.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-script.min.js \
     > public/lib/coffee-script.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/less.js/1.3.3/less.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min.js \
     > public/lib/less.js
 curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.0/dropbox.min.js \
     > public/lib/dropbox.min.js
