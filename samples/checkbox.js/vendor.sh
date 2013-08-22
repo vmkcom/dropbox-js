5681a8e4834cbefac39600ab0b2dba47edc0663c
Victor Costan <costan@gmail.com>
2013-08-22 00:34:28 -0700

    Update dropbox.js version references to 0.10.1.

--- samples/checkbox.js/vendor.sh
+++ samples/checkbox.js/vendor.sh
@@ -9,11 +9,11 @@ curl --fail https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-sc
     > public/lib/coffee-script.js
 curl --fail https://cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min.js \
     > public/lib/less.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.0/dropbox.min.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.1/dropbox.min.js \
     > public/lib/dropbox.min.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.0/dropbox.js \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.1/dropbox.js \
     > public/lib/dropbox.js
-curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.0/dropbox.min.map \
+curl --fail https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.10.1/dropbox.min.map \
     > public/lib/dropbox.min.map
 curl --fail https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js \
     > public/lib/html5shiv.js
